5965b1f148273f53e0f8ab94ce69809b4a39f076
Tamara Nawrocki <sv42028707@idat.edu.pe>
2021-09-07 14:56:41 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+for (let i = 0; i < 21; i++) {
+  if (i % 2 == 0) {
+    console.log(`${i} es par`);
+  } else {
+    console.log(`${i} es impar`);
+  }
+}
+
+for (let i = 0; i < 11; i++) {
+  console.log(`${i} * 9 = ${i * 9}`);
+}
+
+for (let i = 1; i < 11; i++) {
+  for (let j = 1; j < 11; j++) {
+    console.log(`${i} * ${j} = ${i * j}`);
+  }
+}
